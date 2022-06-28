ff3f9d8dd05339401cdb250fa021a245b786facf
yangtingting123456 <3048903923@qq.com>
2022-06-28 17:25:45 +0800

    zantao commit bug code second commit

--- homework/homework_20220622/homework_20220622.py
+++ homework/homework_20220622/homework_20220622.py
@@ -9,20 +9,33 @@ print(driver_path)
 driver = webdriver.Chrome(executable_path=driver_path)
 driver.get('http://47.107.178.45/zentao/www/index.php?m=user&f=login ')
 driver.maximize_window()
-driver.implicitly_wait(30)
+driver.implicitly_wait(10)
 #1 登录
 driver.find_element(By.CSS_SELECTOR,'input[type="text"]').send_keys("test01")
 driver.find_element(By.CSS_SELECTOR,'input[type="password"]').send_keys("newdream123")
 driver.find_element(By.CSS_SELECTOR,'button[type="submit"]').click()
 #2 进入测试-bug-提bug模块
 driver.find_element(By.LINK_TEXT,'测试').click()
+
+#3 输入bug信息
 driver.find_element(By.CSS_SELECTOR,'li[data-id="bug"]').click()
 driver.find_element(By.LINK_TEXT,'提Bug').click()
-#3 输入bug信息
-#3.2 选择项目
-bug_text = driver.find_element(By.CSS_SELECTOR,'iframe.ke-edit-iframe')
-driver.switch_to.frame(bug_text)
-driver.find_element(By.ID,'div#product_chosen').click()
+#4 选择项目
+driver.find_element(By.CSS_SELECTOR,'div#product_chosen').click()
+driver.find_elements(By.CSS_SELECTOR,'div.chosen-drop ul.chosen-results li:nth-child(3)')[0].click()
+#5 所属模块
+time.sleep(1)
+driver.find_elements(By.CSS_SELECTOR,'div#moduleIdBox div')[0].click()
+driver.find_element(By.CSS_SELECTOR,'ul.chosen-results li:nth-child(7)').click()
+#6 所属项目
+driver.find_element(By.CSS_SELECTOR,'span#projectIdBox').click()
+driver.find_elements(By.CSS_SELECTOR,'div.chosen-drop ul.chosen-results li:nth-child(2)')[2].click()
+#影响版本
+driver.find_elements(By.CSS_SELECTOR,'ul.chosen-choices')[0].click()
+driver.find_elements(By.CSS_SELECTOR,'ul.chosen-results li.active-result')[16].click()
 
+
+# bug_text = driver.find_element(By.CSS_SELECTOR,'iframe.ke-edit-iframe')
+# driver.switch_to.frame(bug_text)
 time.sleep(30)
 driver.quit()
