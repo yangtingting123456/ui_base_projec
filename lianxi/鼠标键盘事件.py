407b03bd30c05c76ea47e2abccba6e4d525883ff
yangtingting123456 <3048903923@qq.com>
2022-07-01 17:56:34 +0800

    鼠标键盘操作 first  commit

--- lianxi/鼠标键盘事件.py
+++ lianxi/鼠标键盘事件.py
@@ -1,5 +1,12 @@
 # -- coding: utf-8 --
 # @Time : 2022/7/1 9:13
 # @Author : siyu.yang
-# @File : 鼠标键盘事件.py
-# @Software: PyCharm
+# 在实际的web产品测试中，对于鼠标的操作，不单单只有click(),有时候还要用到
+# 右击、双击、拖动等操作，这些操作包含在ActionChains 类中。
+# context_click() :右击
+# double_click()      :双击
+# drag_and_drop()      :拖动
+# move_to_element()    :鼠标移动到一个元素上
+# click_and_hold()     : 按下鼠标左键在一个元素上
+#鼠标右击导包
+from selenium.webdriver.common.action_chains import ActionChains
